6b39ee1bc7b81735de701b2ab4b3fac0da7dc594
Hants Williams <hantsawilliams@gmail.com>
2024-09-23 19:52:27 -0400

    simple

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+## Create a Flask app
+app = Flask(__name__)
+
+## Define a route
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Hello, World from Hants Williams!"
+
+## run on port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
